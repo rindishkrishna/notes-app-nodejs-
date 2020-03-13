e0c4ad7f9f545d43bf620a732cc163290bf187f6
Rindish krishna <rkatonline@gmail.com>
2020-03-13 15:15:03 +0530

    done

--- Routes/CustomerRoute.js
+++ Routes/CustomerRoute.js
@@ -16,7 +16,7 @@ Router2.get('/:id',asyncvalidator(async (req,res)=>{
     res.send(gen);
 }));
 
-Router2.post('/',asyncvalidator([check('name').isLength({min:3})],async (req,res)=>{
+Router2.post('/',[check('name').isLength({min:3})],asyncvalidator(async (req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()) return res.status(422).json(errors.array() );
     const gen = new Customer({
@@ -34,13 +34,13 @@ Router2.put('/:id',asyncvalidator(async (req,res)=>{
         isGold: req.body.isGold,
         phone: req.body.phone
     }, { new: true });
-    if(!gen) return res.status(404).send("mot found");
+    if(!gen) return res.status(404).send("not found");
     res.send(gen);
 }));
 
 Router2.delete('/:id',asyncvalidator(async (req,res)=>{
     const gen = await Customer.findByIdAndDelete(req.params.id );
-    if(!gen) return res.status(404).send("mot found");
+    if(!gen) return res.status(404).send("not found");
     res.send(gen);
 }));
 
