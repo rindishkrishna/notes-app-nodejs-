abf0c69877040c7599061c0d87294e9ba73032ad
Rindish krishna <rkatonline@gmail.com>
2020-03-14 22:42:03 +0530

    new notes app

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app =express();
 const logger =require('./Startup/Logging');
 const path =require('path');
-const notes =require('./Models/Notes');
 
 require('./Startup/Config')();
 require('./Startup/Prod')(app);
@@ -11,10 +10,6 @@ require('./Startup/Router')(app);
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug');
-app.get('/',async function (req, res) {
-    const note = await notes.find();
-    res.render('index', { title: 'Hey', message: note[0].text })
-});
 
 const port = process.env.PORT;
 app.listen(port,()=>{
--- Startup/Router.js
+++ Startup/Router.js
@@ -2,6 +2,7 @@ const error = require('../Middleware/Errors');
 const register = require('../Routes/RegisterRoute');
 const login = require('../Routes/LoginRoute');
 const notes= require('../Routes/NotesRoute');
+const admin= require('../Routes/Admin');
 const bodyParser = require('body-parser');
 module.exports=function (app) {
     app.use(bodyParser.json());
@@ -9,5 +10,6 @@ module.exports=function (app) {
     app.use('/Register', register);
     app.use('/login', login);
     app.use('/notes', notes);
+    app.use('/admin', admin);
     app.use(error);
 };
